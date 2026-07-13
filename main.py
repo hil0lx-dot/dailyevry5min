75610af3f5aa3041a36d60533649b12612ec7946
hil0lx-dot <hil0lx52@gmail.com>
2026-07-13 05:50:41 -0700

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ SPAM_CHANNEL_ID = "1488606312563736646"
 
 tokens = {
     "Sentinel-1": {"token": os.getenv("TOKEN_ONE"), "channel": VC_ONE_ID, "mobile": True, "spam": True},
-    "Sentinel-2": {"token": os.getenv("TOKEN_TWO"), "channel": VC_TWO_ID, "mobile": False, "spam": True},
+    "Sentinel-2": {"token": os.getenv("TOKEN_TWO"), "channel": VC_TWO_ID, "mobile": False, "spam": False},
     "Sentinel-3": {"token": os.getenv("TOKEN_THREE"), "channel": VC_THREE_ID, "mobile": True, "spam": True}
 }
 
