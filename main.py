7f6503318c66b7253fde5d53f36938018a9d5062
hil0lx-dot <hil0lx52@gmail.com>
2026-07-14 17:06:24 -0700

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ VC_THREE_ID = "1488606312563736646"
 SPAM_CHANNEL_ID = "1488606312563736646"
 
 tokens = {
-    "Sentinel-1": {"token": os.getenv("TOKEN_ONE"), "channel": VC_ONE_ID, "mobile": True, "spam": False},
+    "Sentinel-1": {"token": os.getenv("TOKEN_ONE"), "channel": VC_ONE_ID, "mobile": True, "spam": True},
     "Sentinel-2": {"token": os.getenv("TOKEN_TWO"), "channel": VC_TWO_ID, "mobile": False, "spam": False},
     "Sentinel-3": {"token": os.getenv("TOKEN_THREE"), "channel": VC_THREE_ID, "mobile": True, "spam": False}
 }
@@ -40,7 +40,8 @@ user_current_vc = None  # Tracks your current voice channel globally via gateway
 def spammer_worker(token, name):
     if not token: return
     header = {"Authorization": token.strip()}
-    payload = {"content": "slm lol <:11pm_aaaaaaaaa:1062102316423073893> "}
+    payload = {"content": "​
+ "}
     
     while True:
         try:
